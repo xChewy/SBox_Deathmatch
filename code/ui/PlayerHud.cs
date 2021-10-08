9a7e5b822c3da6566db7b13721a341e57b58e9e5
xChewy <eagis77@hotmail.com>
2021-10-08 11:13:27 -0400

    Added ammo to HUD (unused currently)

--- code/ui/PlayerHud.cs
+++ code/ui/PlayerHud.cs
@@ -6,15 +6,21 @@ public class PlayerHud : Panel
 {
 	private Label HealthIcon;
 	private Label Health;
+	private Label AmmoIcon;
+	private Label Ammo;
 
 	public PlayerHud()
 	{
 		StyleSheet.Load( "/ui/PlayerHud.scss" );
 
 		Panel healthBack = Add.Panel( "healthBG" );
+		Panel ammoBack = Add.Panel( "ammoBG" );
 		
-		HealthIcon = healthBack.Add.Label( "Health: ", "healthText2" );
-		Health = healthBack.Add.Label( "0", "healthText" );
+		HealthIcon = healthBack.Add.Label( "Health: ", "healthText" );
+		Health = healthBack.Add.Label( "100", "text" );
+
+		AmmoIcon = ammoBack.Add.Label( "Ammo: ", "ammoText" );
+		Ammo = ammoBack.Add.Label( "0", "text" );
 	}
 
 	public override void Tick()
